3fcbed3e208ccca9055409bacf959b436a09af01
Jean Carlos de Almeida <jeanjnx@gmail.com>
2019-05-04 16:36:25 -0300

    studys

---
+++ filter-handler/service/s3Service.js
@@ -0,0 +1,48 @@
+const AWS = require('aws-sdk')
+AWS.config.update({
+    region: 'us-east-1'
+})
+
+const BUCKET = 'nanoservices-imagens-black-whitejeanjnx'
+
+const s3 = new AWS.S3();
+
+const getObject = (bucket, key) => {
+    return new Promise((res, rej) => {
+        s3.getObject({
+            Bucket: bucket,
+            Key: key
+        }, (err, data) => {
+            if(err){
+                return rej(err);
+            }
+            return res(data.Body)
+        })
+    })
+}
+
+const putObject = (buffer, filename) => {
+    console.log('Buffer:');
+    console.log(buffer);
+    
+    return new Promise((res, rej) => {
+        s3.putObject({
+            Bucket: BUCKET,
+            Key: 'bw-' + filename,
+            Body: buffer
+        }, (err, data) => {
+            if(err){
+                return rej(err);
+            }
+            return res({
+                bucket: BUCKET,
+                key: 'bw-' + filename
+            });
+        })
+    })
+}
+
+module.exports = {
+    getObject: getObject,
+    putObject: putObject
+}
