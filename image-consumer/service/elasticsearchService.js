dc74686d96bf51c9b42e3b6bc5bb3613bc6712e8
Jean Carlos de Almeida <jeanjnx@gmail.com>
2019-05-05 11:23:39 -0300

    migração apiVersion para 6.7

--- image-consumer/service/elasticsearchService.js
+++ image-consumer/service/elasticsearchService.js
@@ -1,6 +1,6 @@
 const elasticsearch = require('elasticsearch');
 const client = new elasticsearch.Client({
-    apiVersion: '6.4',
+    apiVersion: '6.7',
     host: 'https://vpc-elasticsearch-cluster-onkh7n3bbbv2gughzsnb6pw57q.us-east-1.es.amazonaws.com',
 });
 
